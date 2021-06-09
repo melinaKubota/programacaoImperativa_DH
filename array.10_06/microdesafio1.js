36c0a47c8024a5cf311d95024c1e4905a378e72e
Melina Kubota <81205555+melinaKubota@users.noreply.github.com>
2021-06-09 19:26:33 -0300

    Add files via upload

---
+++ array.10_06/microdesafio1.js
@@ -0,0 +1,16 @@
+let compras = ['arroz', 'feijao', 'batata', 'macarrrao']
+compras.join ('')/* juntas os eementos  */
+console.log("Reultado do join: " + "junta os elementos")
+
+compras.pop ('')/* elimina o ultimo */
+console.log("Reultado do .pop: " + "elimina o ultimo elemento da array")
+
+compras.push ('') /* adinciona mais elementos ao final */
+console.log("Reultado do .push: " + "adiciona mais um elemento ao final da array")
+
+compras.shift ('') /* elimina o primeiro */
+console.log("Reultado do .shift: " + "elimina o primeiro elemento")
+
+compras.unshift ('') /*  adiciona elemtno no inicio */
+console.log("Reultado do .unshift: " + "adiciona elemento no inicio")
+
