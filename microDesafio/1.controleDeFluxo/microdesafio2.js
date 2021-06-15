ae0077344e114eb4e49d37b1a4436c009cb696d0
Melina Kubota <81205555+melinaKubota@users.noreply.github.com>
2021-06-14 21:31:39 -0300

    entrega dos microDesafios

---
+++ microDesafio/1.controleDeFluxo/microdesafio2.js
@@ -0,0 +1,7 @@
+let linguagem=""; 
+
+if(){
+    console.log('Estou aprendendo')
+}else{
+    console.log('Aprender mais tarde')   
+}
---
+++ microDesafio/2.array_10_06/microdesafio1.js
@@ -0,0 +1,12 @@
+let compras=['banana', 'uva', 'laranja'];
+
+console.log('O join coloca o que você pedir entre cada inserção do array:')
+console.log(compras.join('-'));
+console.log ('O pop exclui o ultimo item:')
+console.log(compras.pop());
+console.log ('O push coloca essa nova variavel no array:')
+console.log(compras.push('ovo'));
+console.log ('O shift remove o primeiro elemento da lista e diz qual foi')
+console.log(compras.shift());
+console.log('O unshift acrescenta um novo elemento colocado na string:')
+console.log(compras.unshift('pera'));
