f05893e5c92ca4ff5b9c64441f8a6ccb6603361c
Melina Kubota <81205555+melinaKubota@users.noreply.github.com>
2021-06-17 11:50:44 -0300

    Checkpoint arrow functions
    Transforme as funções em arrow functions

---
+++ microDesafio/4.arrow_17_06/microDesafio1.js
@@ -0,0 +1,16 @@
+                /* TRANSFORME A SEGUINTE FUNÇÃO EM ARROWS FUNCTIONS */
+                /* function print (mensagem){
+                    console.log(mensagem)
+                }
+                print ("Olá, bom dia") */
+
+let print = () => console.log("Olá, bom dia")
+print ();
+
+                /* function soma(n1, n2){
+                    return n1 + n2
+                }
+                console.log(soma(10,10))  */
+
+let soma = (n1, n2) => console.log(n1 + n2)
+soma (10,10);
