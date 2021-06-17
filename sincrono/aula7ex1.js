1b5092d1dd737b4291623cd6080352e8feb0bf47
Melina Kubota <81205555+melinaKubota@users.noreply.github.com>
2021-06-17 16:38:12 -0300

    aula7
    atividades feitas durante a 7 - arrows

---
+++ sincrono/aula7ex1.js
@@ -0,0 +1,15 @@
+//função declarativa
+/* function soma(a,b){
+    return a + b; 
+}
+
+console.log(soma(33,55)); */
+//função anonima
+/* let soma = function(a,b){
+    return a+b;
+}
+console.log(soma(33,55)); */
+
+//arrow function
+let soma=(a,b)=> a + b;
+console.log(soma(33,55));
